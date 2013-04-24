e8f49bd939babe816a8a2cbb87adddb79bcdfffd
Anthony Castelli <mostlyanthony@gmail.com>
2013-04-24 11:49:43 -0700

    Initial Commit

---
+++ Anthony/ACAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ACAppDelegate.h
+//  Anthony
+//
+//  Created by Anthony Castelli on 4/24/13.
+//  Copyright (c) 2013 Emerys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Anthony/ACViewController.m
@@ -0,0 +1,29 @@
+//
+//  ACViewController.m
+//  Anthony
+//
+//  Created by Anthony Castelli on 4/24/13.
+//  Copyright (c) 2013 Emerys. All rights reserved.
+//
+
+#import "ACViewController.h"
+
+@interface ACViewController ()
+
+@end
+
+@implementation ACViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
