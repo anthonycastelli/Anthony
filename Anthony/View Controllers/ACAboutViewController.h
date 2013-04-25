83a25041febfc4f8a0730dbe9767537584118938
Anthony Castelli <mostlyanthony@gmail.com>
2013-04-24 18:44:56 -0700

    Cell Animations Complete

--- Anthony/View Controllers/ACAboutViewController.h
+++ Anthony/View Controllers/ACAboutViewController.h
@@ -8,6 +8,32 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum {
+    ACAboutActionEmerys = 0,
+    ACAboutActionTwitter = 1,
+    ACAboutActionAppDotNet,
+    ACAboutActionDribbble,
+    ACAboutActionGitHub
+} ACAboutAction;
+
 @interface ACAboutViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *background;
+@property (weak, nonatomic) IBOutlet UIButton *backButton;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UIButton *emerys;
+@property (weak, nonatomic) IBOutlet UIButton *twitter;
+@property (weak, nonatomic) IBOutlet UIButton *adn;
+@property (weak, nonatomic) IBOutlet UIButton *dribbble;
+@property (weak, nonatomic) IBOutlet UIButton *github;
+
+- (IBAction)back:(id)sender;
+- (IBAction)emerys:(id)sender;
+- (IBAction)twitter:(id)sender;
+- (IBAction)adn:(id)sender;
+- (IBAction)dribbble:(id)sender;
+- (IBAction)github:(id)sender;
+
+- (void)openAction:(ACAboutAction)action;
 
 @end
--- Anthony/View Controllers/ACAboutViewController.m
+++ Anthony/View Controllers/ACAboutViewController.m
@@ -9,18 +9,83 @@
 #import "ACAboutViewController.h"
 
 @interface ACAboutViewController ()
-
+- (void)shakeView:(UIView *)viewToShake;
 @end
 
 @implementation ACAboutViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-	
+    
+    UISwipeGestureRecognizer *swipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(back:)];
+    [swipe setDirection:UISwipeGestureRecognizerDirectionRight];
+    [self.view addGestureRecognizer:swipe];
+    
+	NSString *about = @"Anthony Castelli about string";
+    NSDictionary *defaultAttributes = @{NSForegroundColorAttributeName: [UIColor whiteColor]};
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:about attributes:defaultAttributes];
+    [self.textView setAttributedText:attributedString];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Options
+
+- (IBAction)back:(id)sender {
+    [self shakeView:self.backButton];
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (IBAction)emerys:(id)sender {
+    [self shakeView:self.emerys];
+    [self openAction:ACAboutActionEmerys];
+}
+
+- (IBAction)twitter:(id)sender {
+    [self shakeView:self.twitter];
+    [self openAction:ACAboutActionTwitter];
+}
+
+- (IBAction)adn:(id)sender {
+    [self shakeView:self.adn];
+    [self openAction:ACAboutActionAppDotNet];
+}
+
+- (IBAction)dribbble:(id)sender {
+    [self shakeView:self.dribbble];
+    [self openAction:ACAboutActionDribbble];
+}
+
+- (IBAction)github:(id)sender {
+    [self shakeView:self.github];
+    [self openAction:ACAboutActionGitHub];
+}
+
+- (void)openAction:(ACAboutAction)action {
+    
+}
+
+#pragma mark - Animations
+
+- (void)shakeView:(UIView *)viewToShake {
+    CGFloat t = 4.0;
+    CGAffineTransform translateRight  = CGAffineTransformTranslate(CGAffineTransformIdentity, t, 0.0);
+    CGAffineTransform translateLeft = CGAffineTransformTranslate(CGAffineTransformIdentity, -t, 0.0);
+    
+    viewToShake.transform = translateLeft;
+    
+    [UIView animateWithDuration:0.07 delay:0.0 options:UIViewAnimationOptionAutoreverse | UIViewAnimationOptionRepeat animations:^{
+        [UIView setAnimationRepeatCount:2.0];
+        viewToShake.transform = translateRight;
+    } completion:^(BOOL finished) {
+        if (finished) {
+            [UIView animateWithDuration:0.05 delay:0.0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+                viewToShake.transform = CGAffineTransformIdentity;
+            } completion:NULL];
+        }
+    }];
 }
 
 @end
