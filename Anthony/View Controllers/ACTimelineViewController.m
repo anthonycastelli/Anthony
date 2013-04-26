001f510ad7f112bb184c7e847f637db7f0fcde06
Anthony Castelli <mostlyanthony@gmail.com>
2013-04-25 19:06:25 -0700

    Timeline Complete

--- Anthony/View Controllers/ACTimelineViewController.m
+++ Anthony/View Controllers/ACTimelineViewController.m
@@ -21,9 +21,8 @@
     
     [self.tableView setBackgroundColor:[UIColor blackColor]];
     
-    [self animateView:self.timeline toPoint:CGPointMake(160, 30) withDelay:0.3];
-    [self animateView:self.backButton toPoint:CGPointMake(30, 30) withDelay:0.35];
-    
+    [self animateView:self.timeline toPoint:CGPointMake(160, 30) withDelay:0.4];
+    [self animateView:self.backButton toPoint:CGPointMake(30, 30) withDelay:0.3];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,13 +30,12 @@
 }
 
 - (IBAction)back:(id)sender {
-    
-    [self animateView:self.backButton toPoint:CGPointMake(350, 30) withDelay:0.0];
-    [self animateView:self.timeline toPoint:CGPointMake(480, 30) withDelay:0.03];
+    [self animateView:self.backButton toPoint:CGPointMake(350, 30) withDelay:0.1];
+    [self animateView:self.timeline toPoint:CGPointMake(480, 30) withDelay:0.0];
     
     [self performBlock:^{
         [self.navigationController popViewControllerAnimated:YES];
-    } afterDelay:0.3];
+    } afterDelay:0.1];
 }
 
 #pragma mark - Animations
@@ -83,21 +81,30 @@
     [cell.contentView setBackgroundColor:[UIColor blackColor]];
     
     
+    [self performBlock:^{
+        [cell.background setImage:[UIImage imageNamed:@"timelines_background"]];
+        [cell bounceImage:cell.background InToPoint:CGPointMake(160, 1338) withDelay:0];
+    } afterDelay:0.2];
     
-    [cell.background setImage:[UIImage imageNamed:@"timelines_background"]];
-    [cell bounceImage:cell.background InToPoint:CGPointMake(160, 1338) withDelay:0.4];
+    [self performBlock:^{
+        [cell.dimonds setImage:[UIImage imageNamed:@"timelines_dimonds"]];
+        [cell bounceImage:cell.dimonds InToPoint:CGPointMake(160, 1338) withDelay:0];
+    } afterDelay:0.25];
     
-    [cell.dimonds setImage:[UIImage imageNamed:@"timelines_dimonds"]];
-    [cell bounceImage:cell.dimonds InToPoint:CGPointMake(160, 1338) withDelay:0.45];
+    [self performBlock:^{
+        [cell.types setImage:[UIImage imageNamed:@"timelines_types"]];
+        [cell bounceImage:cell.types InToPoint:CGPointMake(160, 1338) withDelay:0];
+    } afterDelay:0.3];
     
-    [cell.types setImage:[UIImage imageNamed:@"timelines_types"]];
-    [cell bounceImage:cell.types InToPoint:CGPointMake(160, 1338) withDelay:0.50];
+    [self performBlock:^{
+        [cell.lines setImage:[UIImage imageNamed:@"timelines_lines"]];
+        [cell bounceImage:cell.lines InToPoint:CGPointMake(160, 1338) withDelay:0];
+    } afterDelay:0.35];
     
-    [cell.lines setImage:[UIImage imageNamed:@"timelines_lines"]];
-    [cell bounceImage:cell.lines InToPoint:CGPointMake(160, 1338) withDelay:0.55];
-    
-    [cell.info setImage:[UIImage imageNamed:@"timelines_info"]];
-    [cell bounceImage:cell.info InToPoint:CGPointMake(160, 1338) withDelay:0.60];
+    [self performBlock:^{
+        [cell.info setImage:[UIImage imageNamed:@"timelines_info"]];
+        [cell bounceImage:cell.info InToPoint:CGPointMake(160, 1338) withDelay:0];
+    } afterDelay:0.4];
 }
 
 @end
