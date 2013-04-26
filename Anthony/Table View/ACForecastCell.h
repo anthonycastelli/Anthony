685232e00a286310594092e33f3da4ec06fe14f7
Anthony Castelli <mostlyanthony@gmail.com>
2013-04-25 23:34:52 -0700

    Fixed enum types

--- Anthony/Table View/ACForecastCell.h
+++ Anthony/Table View/ACForecastCell.h
@@ -8,12 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum {
+    ACForecastDayColorBlue,
+    ACForecastDayColorGreen,
+    ACForecastDayColorYellow,
+    ACForecastDayColorOrange
+} ACForecastDayColor;
+
 @interface ACForecastCell : UITableViewCell
 
 @property (weak, nonatomic) IBOutlet UIImageView *condition;
 @property (weak, nonatomic) IBOutlet UILabel *temp;
 @property (weak, nonatomic) IBOutlet UILabel *day;
+@property (weak, nonatomic) IBOutlet UIImageView *background;
 
+- (void)setForecastColor:(ACForecastDayColor)color;
 - (void)bounceView:(UIView *)view InToPoint:(CGPoint)point withDelay:(NSTimeInterval)delay;
 
 @end
--- Anthony/Table View/ACForecastCell.m
+++ Anthony/Table View/ACForecastCell.m
@@ -16,6 +16,25 @@
     // Configure the view for the selected state
 }
 
+- (void)setForecastColor:(ACForecastDayColor)color {
+    switch (color) {
+        case ACForecastDayColorBlue:
+            [self.background setImage:[UIImage imageNamed:@"weather_current_blue"]];
+            break;
+        case ACForecastDayColorGreen:
+            [self.background setImage:[UIImage imageNamed:@"weather_current_green"]];
+            break;
+        case ACForecastDayColorYellow:
+            [self.background setImage:[UIImage imageNamed:@"weather_current_yellow"]];
+            break;
+        case ACForecastDayColorOrange:
+            [self.background setImage:[UIImage imageNamed:@"weather_current_orange"]];
+            break;
+        default:
+            break;
+    }
+}
+
 - (void)bounceView:(UIView *)view InToPoint:(CGPoint)point withDelay:(NSTimeInterval)delay {
     [self performBlock:^{
         NSString *keyPath = @"position.x";
